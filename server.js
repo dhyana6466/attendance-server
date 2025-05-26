d77d4eedba5d70b6fcac4353b89b1b97e3758e49
dhyana6466 <dhyanapatel6466@gmail.com>
2025-05-26 11:28:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.post('/submit', (req, res) => {
   res.json({ message: 'Saved to Excel ✅' });
 });
 
-app.listen(3000, () => {
-  console.log('✅ Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('✅ Server running on port ${PORT}');
 });
